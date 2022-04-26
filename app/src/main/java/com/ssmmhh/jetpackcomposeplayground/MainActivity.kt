ebaeee3ef0d19a360a8e10c975200a43d5ed21a2
mahdi <ssmmhh1382@gmail.com>
2022-04-27 00:32:17 +0430

    finish layout lesson in android compose tutorial

--- app/src/main/java/com/ssmmhh/jetpackcomposeplayground/MainActivity.kt
+++ app/src/main/java/com/ssmmhh/jetpackcomposeplayground/MainActivity.kt
@@ -3,13 +3,19 @@ package com.ssmmhh.jetpackcomposeplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ssmmhh.jetpackcomposeplayground.ui.theme.JetpackComposePlaygroundTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MessageCard(Message.TEST_MESSAGE_1)
                 }
             }
         }
@@ -30,14 +36,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(mgs: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape),
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = stringResource(R.string.profile_picture)
+        )
+        Spacer(modifier = Modifier.size(8.dp))
+        Column {
+            Text(text = mgs.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = mgs.text)
+        }
+
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackComposePlaygroundTheme {
-        Greeting("Android")
+        MessageCard(
+            Message.TEST_MESSAGE_1
+        )
+    }
+}
+
+data class Message(
+    val author: String,
+    val text: String
+) {
+    companion object {
+        val TEST_MESSAGE_1 = Message(
+            author = "Mahdi",
+            text = "Hey are u up for tonight?"
+        )
     }
 }
